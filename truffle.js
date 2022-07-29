c6a8fc2b7a627036e3c6815227ecd54fcf7c84dc
bwuzahng <bwuzhang@gmail.com>
2022-07-29 15:28:22 -0400

    Fix initial error from scaffolding code.

--- truffle.js
+++ truffle.js
@@ -1,14 +1,20 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const fs = require('fs');
+let secrets;
+
+if (fs.existsSync('secrets.json')) {
+ secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+}
 
 module.exports = {
   networks: {
     development: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+        return new HDWalletProvider(secrets.mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 4500000
     }
   },
   compilers: {
